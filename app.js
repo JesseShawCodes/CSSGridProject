bb4f4882ee0106fd861918c362d09ecd49b4b1b9
Jesse Shaw <jdshaw1987@gmail.com>
2017-08-08 15:25:45 -0400

    Updated and implemented a form prototype

--- app.js
+++ app.js
@@ -1,3 +1,21 @@
+//Default Objects
+const resources = [{
+                    descriptionVal: "MDN HTML Homepage",
+                    linkVal: "https://developer.mozilla.org/en-US/docs/Web/HTML",
+                    dropdownVal: "html"
+                },
+                 {
+                     descriptionVal: "MDN CSS Homepage",
+                     linkVal: "https://developer.mozilla.org/en-US/docs/Learn/CSS/Introduction_to_CSS",
+                     dropdownVal: "css"
+                 }
+                ]
+
+for (var i = 0; i < resources.length; i++) {
+    $("#grid").append(`<section class="${resources[i].dropdownVal}"><h2>${resources[i].descriptionVal}</h2><a href="${resources[i].linkVal}">Click Here</a></section>`);
+}
+
+
 //Function to toggle subjects//
 function boxToggle(subject) {
     let a = "." + subject;
@@ -20,7 +38,11 @@ $(".inputcall").on("click", function(e) {
 
 //Input Form Submit
 
-$("form").submit(function() {
-    var x = $("form").find(".description").val();
+$("form").on("submit", function(e) {
+    e.preventDefault();
+    var descriptionVal = $("form").find(".description").val();
+    var linkVal = $("form").find(".formlink").val();
+    var dropdownVal = $("form").find(".dropdown").val();
+    $("#grid").append(`<section class="${dropdownVal}"><h2>${descriptionVal}</h2><a href="${linkVal}">Click Here</a></section>`);
 });
 
